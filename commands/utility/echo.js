925d4b4323a17053a0b8b22ac77a3ab1c61e1751
Tyler Nevell <tylernevell@gmail.com>
2026-01-28 14:56:50 -0500

    run through examples

---
+++ commands/utility/echo.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('Replies with your input!')
+    .addStringOption((option) =>
+      option
+        .setName('input')
+        .setDescription('The input to echo back')
+        .setRequired(true),
+    )
+    .addChannelOption((option) =>
+      option.setName('channel').setDescription('The channel to echo into'),
+    )
+    .addBooleanOption((option) =>
+      option
+        .setName('ephemeral')
+        .setDescription('Whether or not the echo should be ephemeral'),
+    ),
+};
